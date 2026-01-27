322162d1bff38a200dc5b07badb4ba39d6ac1361
Luno <2022585923@qq.com>
2026-01-28 00:35:14 +0800

    分布式锁-Redis的分布式锁版本3

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -25,6 +28,16 @@ public class SimpleRedisLock implements ILock{
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    // 加载释放锁的脚本
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
+
+
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取当前线程的标识: UUID + 线程ID
@@ -37,6 +50,16 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
+        // 调用 Lua 脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
+    }
+
+    /*@Override
+    public void unLock() {
         // 获取当前线程的标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
@@ -45,5 +68,5 @@ public class SimpleRedisLock implements ILock{
             // 释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
