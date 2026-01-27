cdde1bfd5efb323f3af9787bf40fd2631f619cd8
Luno <2022585923@qq.com>
2026-01-28 00:33:04 +0800

    分布式锁-Redis的分布式锁版本2

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -9,15 +10,15 @@ public class SimpleRedisLock implements ILock{
     // 锁的名称
     private String name;
 
-    // 前缀
+    // 锁的前缀
     private static final String KEY_PREFIX = "lock:";
+    // 线程标识的前缀
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";   //传入true，返回短UUID（去掉横线）
 
     private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 创建锁的构造器
-     * @param name
-     * @param stringRedisTemplate
      */
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -26,17 +27,23 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public boolean tryLock(long timeoutSec) {
-        // 获取当前线程的标识
-        long threadId = Thread.currentThread().getId();
+        // 获取当前线程的标识: UUID + 线程ID
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId+ "", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);    // 避免装箱问题，空指针
     }
 
     @Override
     public void unLock() {
-        // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取当前线程的标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断是否是当前线程的锁
+        if(threadId.equals(id)) {
+            // 释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
